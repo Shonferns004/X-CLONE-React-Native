fe529ebaf6ad6049ffb9978e89c7320b4aee0545
Shonferns004 <shawnferns004@gmail.com>
2025-07-01 17:38:20 +0530

    db mongooes created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,20 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+connectDB();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+
+
+app.listen(ENV.PORT, () => {
+    console.log("Server is running on port",ENV.PORT);
 });
 
 
